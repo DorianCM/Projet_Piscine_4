879e9f00cb5aa17e1031dcaf94e57affb0c6bd83
Marvin <bontempsscolaire@gmail.com>
2021-10-24 11:40:59 +0200

    page déconnexion

--- API/authentification/disconnect.php
+++ API/authentification/disconnect.php
@@ -3,11 +3,6 @@ session_start();
 session_unset();
 session_destroy();
 
-echo "<p> Vous êtes bien déconnecter ! ";
-echo "<a href='../../'>Retourner à l'accueil : </a>";
-
-
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,8 +10,10 @@ echo "<a href='../../'>Retourner à l'accueil : </a>";
     <meta charset="UTF-8">
     <title>Déconnexion</title>
     <link rel="stylesheet" type="text/css" href="../../css/entete.css">
+    <link href="https://fonts.googleapis.com/css2?family=Andada+Pro&display=swap" rel="stylesheet">
+    <meta http-equiv="refresh" content="25; url='../../' "/>
 </head>
-<body>
+<body style='width:100%;text-align: center;'>
     <header>
         <ul class="entetu">
             <li class="entetli"><a class="enteta" onclick = "window.location= '../../'">Accueil</a></li>
@@ -39,5 +36,20 @@ echo "<a href='../../'>Retourner à l'accueil : </a>";
             ?>
         </ul>
     </header>
+    <main style='margin-top:20%;font-family: "Andada Pro", serif;'>
+        <h1>Déconnexion réussie !</h1>
+        <br>
+        <h2>Vous allez être redirigé vers l'accueil</h2>
+    </main>
+    <footer style='position: fixed;background-color: #c3b7a4;width: 100%;height: 6%;bottom: 0px;font-family: "Andada Pro", serif;'>
+        <div style='display: flex;position: center;margin: 8px;'>
+            <div> © </div>
+            <div> | </div>
+            <div>
+                Lycée des métiers de la gastronomie, de l'hôtellerie et des tourismes Georges-Frêche
+            </div>
+        </div>
+    </footer>
+
 </body>
 </html>
